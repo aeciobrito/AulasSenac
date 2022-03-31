aee9e6c07ae756189100eb3f32dbbc2028c4838a
Aecio Brito Senac <aecio.mbrito@senacsp.edu.br>
2022-03-31 20:28:54 -0300

    Projeto demonstrativo static

---
+++ CalculadoraGeral/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculadoraGeral
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BancoDeDados.AdicionarContato("João");
+
+            Funcionario funcionario1 = new Funcionario();
+            funcionario1.BaterOPonto();
+        }
+
+        public class Pessoa
+        {
+            public virtual void BaterOPonto()
+            {
+                Console.WriteLine($"Horário marcado em {System.DateTime.Now}");
+            }
+        }
+
+        public class Funcionario : Pessoa
+        {
+
+        }
+
+        public class Aluno : Pessoa
+        {
+            public override void BaterOPonto()
+            {
+                //antes
+                base.BaterOPonto();
+                //depois
+                Console.WriteLine("Aluno marcou presença");
+            }
+        }
+
+        public class Calculadora
+        {
+            public static int numero;
+
+            public static int Soma(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        public static class BancoDeDados
+        {
+             static List<string> contatos = new List<string>();
+
+            public static void AdicionarContato(string nome)
+            {
+                contatos.Add(nome);
+            }
+        }
+    }
+}
